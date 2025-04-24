97a212fe38576b16ac812494c2127ecee12ca429
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-25 00:43:46 +0530

    learned_ejs

--- miniProject/index.js
+++ miniProject/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-    res.send('Chal raha hai beee');
+    res.render('index');
 });
 
 app.listen(3000);
