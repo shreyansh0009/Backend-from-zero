b4ef1926999cd5f1fc004e6747f3f07f37420bda
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-29 00:05:11 +0530

    update

---
+++ database/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+const userModel = require("./usermodel");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/create", async (req, res) => {
+  const createdUser = await userModel.create({
+    name: "Rubi Gupta",
+    username: "rubig",
+    email: "rubig@gmail.com",
+  });
+  res.send(createdUser);
+});
+
+app.get("/read", async (req, res) => {
+  const users = await userModel.find();
+  res.send(users);
+});
+
+app.get("/update", async (req, res) => {
+  const updatedUser = await userModel.findOneAndUpdate(
+    {
+      username: "rubig",
+    },
+    {
+      username: "rubi123",
+    },
+    {
+      new: true,
+    }
+  );
+  res.send(updatedUser);
+});
+
+app.get("/delete", async (req, res) => {
+    const deletedUser = await userModel.findOneAndDelete({
+        username: "shreyansh0009"
+    });
+    res.send(deletedUser);
+});
+app.listen(3000);
---
+++ database/usermodel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost:27017/userdb`);
+
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: String
+});
+
+module.exports = mongoose.model('User', userSchema);
