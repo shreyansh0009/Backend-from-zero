24e5b9380f8a833c01dd857fff2ce3babd8072e8
shreyansh0009 <saurabhsharraf@gmail.com>
2025-08-01 23:30:06 +0530

    learned referencing in databases

--- dataModeling/models/sub_todo.model.js
+++ dataModeling/models/sub_todo.model.js
@@ -1,5 +1,23 @@
 import mongoose from "mongoose";
 
-const subTodoSchema = mongoose.Schema({}, { timestamps: true });
+const subTodoSchema = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId, // referencing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const subTodo = mongoose.model("subTodo", subTodoSchema);
--- dataModeling/models/todo.model.js
+++ dataModeling/models/todo.model.js
@@ -1,7 +1,27 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-    
+    content: {
+        type: String,
+        required: true,
+    },
+
+    complete: {
+        type: Boolean,
+        default: false,
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId, // referencing
+        ref: "User",
+    },
+
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "subTodo",
+        }
+    ] // Referencing another directory
 
 }, {timestamps: true});
 
