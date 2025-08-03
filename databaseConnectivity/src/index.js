da1708a3d5eb53da803ba16a14f05727065baa78
shreyansh0009 <saurabhsharraf@gmail.com>
2025-08-03 15:24:51 +0530

    successfully connected mongoose from backend without express.(on terminal)

---
+++ databaseConnectivity/src/index.js
@@ -0,0 +1,45 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import mongoose from "mongoose";
+// import express from "express";
+// import { chaiBackend } from "./constants.js";
+
+// const app = express();
+
+// // 1st method of connecting db, 2nd is best and it's in '/db/index.js'
+
+// if (!process.env.MONGODB_URI) {
+//   console.error("MONGODB_URI environment variable is not set!");
+//   process.exit(1);
+// }
+
+// if (!process.env.PORT) {
+//   console.error("PORT environment variable is not set!");
+//   process.exit(1);
+// }
+
+// (async () => {  // iffi fun, that executes asap.
+//   try {
+//     const connectedData = await mongoose.connect(
+//       `${process.env.MONGODB_URI}/${chaiBackend}`
+//     );
+//     console.log("MongoDB connected !!");
+//     console.log(connectedData);
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is running on port: ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Mongo connection error:", error);
+//     throw error;
+//   }
+// })();
+
+
+// 2nd process of connecting db, more professional and effcient.
+import dotenv from "dotenv";
+dotenv.config({path: './.env'});
+import connectDB from "./db/index.js";
+
+connectDB()
