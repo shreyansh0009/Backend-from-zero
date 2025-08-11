54ae04c0ff15afe24b6890b885b1bddf4a7889ea
shreyansh0009 <saurabhsharraf@gmail.com>
2025-08-12 00:55:19 +0530

    builded login functions and hit the endpoint usin axios and got access and refresh token on console

--- userAuthentication_MERN/client/src/pages/Login.jsx
+++ userAuthentication_MERN/client/src/pages/Login.jsx
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
-  return (
-    <div className="flex flex-col space-y-3 m-5 h-72 w-80 p-5 bg-sky-500 border-2 rounded-lg">
-      <div className="space-y-3 flex flex-col justify-center items-center">
-        <label htmlFor="email" className="font-semibold text-lg">Email</label>
-        <input type="email" placeholder="Enter email" className="border-2 rounded-md p-1 w-[97%]"/>
-        <label htmlFor="email" className="font-semibold text-lg">Password</label>
-        <input type="password" placeholder="Enter password" className="border-2 rounded-md p-1 w-[97%]"/>
-      </div>
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
 
-      <button className="mt-3 text-lg bg-emerald-400 rounded-lg p-2 font-semibold cursor-pointer">Login</button>
+  function handleSubmit() {
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      axios
+        .post("https://api.escuelajs.co/api/v1/auth/login", user)
+        .then((res) => {
+          window.alert("Login Successfull!");
+          const access_token = res.data.access_token;
+          const refresh_token = res.data.refresh_token;
+          console.log(res.data);
+          console.log("Access Token: ", access_token);
+          console.log("Refresh Token: ", refresh_token);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+    // setemail("");
+    // setpassword("");
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-zinc-900">
+      <div className="flex flex-col space-y-3 w-80 p-5 bg-gray-500 border-2 rounded-lg">
+        <div className="space-y-3 flex flex-col justify-center items-center">
+          <label htmlFor="email" className="font-semibold text-lg">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Enter email"
+            className="border-2 rounded-md p-1 w-[97%]"
+            value={email}
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+          />
+          <label htmlFor="password" className="font-semibold text-lg">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter password"
+            className="border-2 rounded-md p-1 w-[97%]"
+            value={password}
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+          />
+        </div>
+
+        <button
+          className="mt-3 text-lg bg-emerald-400 rounded-lg p-2 font-semibold cursor-pointer"
+          onClick={handleSubmit}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
